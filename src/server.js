138c7e726f5376235a2203a8971aba5c6be144c9
Leonel <leonel.carrossoni@gmail.com>
2020-08-06 21:42:22 -0300

    Ajustes formulários

--- src/server.js
+++ src/server.js
@@ -20,22 +20,96 @@ const proffys = [
         weekday: [1], 
         time_from: [720], 
         time_to: [1220]
+    },
+    { 
+        name: "João Carrossoni", 
+        avatar: "https://avatars2.githubusercontent.com/u/59970468?s=460&v=4",
+        whatsapp: "899985656",
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject: "Programação",
+        cost: "50", 
+        weekday: [1], 
+        time_from: [720], 
+        time_to: [1220]
     }
 ]
 
+const subjects = [
+    "Artes",
+    "Biologia",
+    "Ciências",
+    "Educação física",
+    "Física",
+    "Geografia",
+    "História",
+    "Matemática",
+    "Português",
+    "Química",
+]
+
+const weekdays = [
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado",
+]
+
+function pageLanding(req, res){
+    return res.render("index.html")
+}
+
+// Funcionalidades
+
+function getSubject(subjectNumber){
+    const position = +subjectNumber -1
+    return subjects[position]
+}
+
+function pageStudy(req, res){
+    const filters = req.query
+    return res.render("study.html", { proffys, filters, subjects, weekdays })
+}
+
+function pageGiveClasses(req, res){
+    const data = req.query
+
+    //se tiver data
+    const isNotEmpty = Object.keys(data).length > 0
+    if (isNotEmpty){
+
+        data.subject = getSubject(data.subject)
+    // adicionar data a lista de proffys
+        proffys.push(data)
+
+        return res.redirect("/study")
+    }
+
+    // se não, mostrar a página
+    return res.render("give-classes.html", { subjects, weekdays})
+}
+
+//Servidor
 const express = require('express')
 const server = express()
 
-server.use(express.static("public"))
-.get("/", (req, res) => {
-    return res.sendFile(__dirname + "/views/index.html")
-})
-.get("/study", (req, res) => {
-    return res.sendFile(__dirname + "/views/study.html")
-})
-.get("/give-classes", (req, res) => {
-    return res.sendFile(__dirname + "/views/give-classes.html")
+//configurar nunjucks (template engine)
+const nunjucks = require('nunjucks')
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true,
 })
 
+
+// Inicio e configuração do servidor
+server.use(express.static("public"))
+// configurar arquivos estáticos (css, scripts, imagens)
+.get("/", pageLanding )
+// rotas de aplicação
+.get("/study", pageStudy)
+.get("/give-classes", pageGiveClasses)
+// start do servidor
 .listen(5500)
 
