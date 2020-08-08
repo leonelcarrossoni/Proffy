626672fe15a6e01173aa2a74b15f4525ce4b88b7
Leonel <leonel.carrossoni@gmail.com>
2020-08-08 15:06:55 -0300

    Relacionando databases

---
+++ src/database/createProffy.js
@@ -0,0 +1,6 @@
+module.exports = function(db, {proffyValue, classValue, classScheduleValue}) {
+    // inserir dados na tabela de teachers
+    await db.run().then() //não precisa do then porque o await já presupões que ele só irá executar a próxima linha depois de concluído
+
+    await db.run().then()
+}
---
+++ src/database/db.js
@@ -0,0 +1,34 @@
+const Database = require('sqlite-async')
+
+function execute (db) {
+    //console.log(db)
+    // criando as tabelas do banco de dados. A função return que garante que o db retorne para o "then"
+    return db.exec(`    
+        CREATE TABLE IF NOT EXISTS proffys (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            avatar TEXT,
+            whatsapp TEXT,
+            bio TEXT
+        );
+
+        CREATE TABLE IF NOT EXISTS classes (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            subject TEXT,
+            cost TEXT,
+            proffy_id INTEGER
+        );
+
+        CREATE TABLE IF NOT EXISTS class_schedule (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            class_id INTEGER,
+            weekday INTEGER,
+            time_from INTEGER,
+            time_to INTEGER
+        );
+    `)
+}
+
+module.exports = Database.open(__dirname + '/database.sqlite').then(execute) //Vai abrir o data base e então executar a função execute...
+
+// node src/database/db.js
